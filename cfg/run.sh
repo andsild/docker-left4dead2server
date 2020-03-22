2f6e650e39b30a954cbad705bc55b2bdf3a4ede4
Anders Sildnes <andsild@posteo.net>
2020-03-23 00:07:51 +0100

    Add missing git fetch etc

--- cfg/run.sh
+++ cfg/run.sh
@@ -2,6 +2,10 @@
 
 set -xe
 
+cd /home/dev/docker-left4dead2server/
+git clean -f
+git fetch --all
+git reset --hard origin/master
 docker build -t left4dead2server .
 # We don't run, don't want to restart gameserver in case of changes
 #docker run -p 27015:27015 -p 27015:27015/udp --env-file /home/dev/.servervars.env left4dead2server:latest
